509977f2370c69b8a34d70e1e9fa4e608cda8750
D1ogoAntunes <diogo.p.antunes04@gmail.com>
2025-03-20 23:14:29 +0000

    Ficheiros Evento Categoria Avaliacao

--- Objetos de Negocio/CategoriaEvento.cs
+++ Objetos de Negocio/CategoriaEvento.cs
@@ -17,5 +17,66 @@ namespace Objetos_de_Negocio
     /// </summary>
     public class CategoriaEvento
     {
+
+        #region Atributos
+
+        /// <summary>
+        /// Atributos declarados para a classe CategoriaEvento
+        /// </summary>
+        private int idCategoria;
+        private string nome;
+
+        #endregion
+
+
+        #region Metodos
+
+        #region Construtores
+
+        /// <summary>
+        /// Construtor padrão que inicia os atributos com valor padrão
+        /// </summary>   
+        public CategoriaEvento()
+        {
+            idCategoria = 0;
+            nome = "";
+        }
+
+        /// <summary>
+        /// Construtor que permite definir todos os atributos ao criar uma instância da classe
+        /// </summary>
+        /// <param name="idCategoria"></param>
+        /// <param name="nome"></param>
+        public CategoriaEvento(int idCategoria, string nome)
+        {
+            this.idCategoria = idCategoria;
+            this.nome = nome;
+        }
+
+        #endregion
+
+
+        #region Propriedades
+
+        /// <summary>
+        /// Propriedade responsavel por definir o id da Categoria do Evento
+        /// </summary>
+        public int IdCategoria
+        {
+            get { return idCategoria; }
+            set { idCategoria = value; }
+        }
+
+        /// <summary>
+        /// Propriedade responsavel por definir o nome da Categoria do Evento
+        /// </summary>
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        #endregion
+        #endregion
     }
 }
--- Objetos de Negocio/Evento.cs
+++ Objetos de Negocio/Evento.cs
@@ -17,5 +17,132 @@ namespace Objetos_de_Negocio
     /// </summary>
     public class Evento
     {
+         #region Atributos
+
+        /// <summary>
+        /// Atributos declarados para a classe Evento
+        /// </summary>
+        private int idEvento;
+        private int idAdmin;
+        private int idCategoria;
+        private string nome;
+        private string descricao;
+        private string localizacao;
+        private DateTime data;
+        
+        //private status;
+
+        #endregion
+
+
+        #region Metodos
+
+        #region Construtores
+
+        /// <summary>
+        /// Construtor padrão que inicia os atributos com valor padrão
+        /// </summary>   
+        public Evento()
+        {
+            idEvento = 0;
+            idAdmin = 0;
+            idCategoria = 0;
+            nome = "";
+            descricao = "";
+            localizacao = "";
+            data = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Construtor que permite definir todos os atributos ao criar uma instância da classe
+        /// </summary>
+        /// <param name="idEvento"></param>
+        /// <param name="idAdmin"></param>
+        /// <param name="idCategoria"></param>
+        /// <param name="nome"></param>
+        /// <param name="descricao"></param>
+        /// <param name="localizacao"></param>
+        /// <param name="data"></param>
+        public Evento(int idEvento, int idAdmin, int idCategoria, string nome, string descricao, string localizacao, DateTime data)
+        {
+            this.idEvento = idEvento;
+            this.idAdmin = idAdmin;
+            this.idCategoria = idCategoria;
+            this.nome = nome;
+            this.descricao = descricao;
+            this.localizacao = localizacao;
+            this.data = data;
+        }
+
+        #endregion
+
+
+        #region Propriedades
+
+        /// <summary>
+        /// Propriedade responsavel por definir o id do Evento
+        /// </summary>
+        public int IdEvento
+        {
+            get { return idEvento; }
+            set { idEvento = value; }
+        }
+
+        /// <summary>
+        /// Propriedade responsavel por definir o id do Admin que criou o Evento
+        /// </summary>
+        public int IdAdmin
+        {
+            get { return idAdmin; }
+            set { idAdmin = value; }
+        }
+
+        /// <summary>
+        /// Propriedade responsavel por definir o id da categoria do Evento
+        /// </summary>
+        public int IdCategoria
+        {
+            get { return idCategoria; }
+            set { idCategoria = value; }
+        }
+
+        /// <summary>
+        /// Propriedade responsavel por definir o nome do Evento
+        /// </summary>
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        /// <summary>
+        /// Propriedade responsavel por definir a descrição do Evento
+        /// </summary>
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+        /// <summary>
+        /// Propriedade responsavel por definir a localização do Evento
+        /// </summary>
+        public string Localizacao
+        {
+            get { return localizacao; }
+            set { localizacao = value; }
+        }
+
+        /// <summary>
+        /// Propriedade responsavel por definir a data do Evento
+        /// </summary>
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        #endregion
+        #endregion
     }
 }
