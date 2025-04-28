4759b8aa30b67c10d360b7f670c27beb52247d8c
D1ogoAntunes <diogo.p.antunes04@gmail.com>
2025-04-28 01:22:16 +0100

    base de dados

--- prisma/migrations/20250428002011_init/migration.sql
+++ prisma/migrations/20250428002011_init/migration.sql
@@ -60,18 +60,6 @@ CREATE TABLE `CategoriaEvento` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `RifaComprada` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `id_utilizador` INTEGER NOT NULL,
-    `id_pagamento` INTEGER NOT NULL,
-    `estado` ENUM('Vencedor', 'SegundoLugar', 'TerceiroLugar', 'Perdedor', 'Pendente') NOT NULL DEFAULT 'Pendente',
-
-    INDEX `id_utilizador`(`id_utilizador`),
-    INDEX `id_pagamento`(`id_pagamento`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
 CREATE TABLE `Evento` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `titulo` VARCHAR(100) NOT NULL,
@@ -119,27 +107,11 @@ CREATE TABLE `Notificacao` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Pagamento` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `id_utilizador` INTEGER NOT NULL,
-    `id_sorteioRifas` INTEGER NOT NULL,
-    `quantidade` INTEGER NOT NULL,
-    `valor` DECIMAL(10, 2) NOT NULL,
-    `metodo_pagamento` ENUM('MBWAY', 'Entidade_Referencia') NOT NULL,
-    `estado` ENUM('Pendente', 'Pago', 'Falhado') NOT NULL DEFAULT 'Pendente',
-    `data_pagamento` TIMESTAMP(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
-
-    INDEX `id_utilizador`(`id_utilizador`),
-    INDEX `id_sorteioRifas`(`id_sorteioRifas`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
 CREATE TABLE `SorteioRifas` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `nome` VARCHAR(100) NOT NULL,
     `preco` DECIMAL(10, 2) NOT NULL,
-    `quantidade` INTEGER NOT NULL,
+    `quantidadeTotal` INTEGER NOT NULL,
     `descricao` TEXT NOT NULL,
     `premio` TEXT NOT NULL,
     `data_sorteio` DATE NOT NULL,
@@ -149,6 +121,34 @@ CREATE TABLE `SorteioRifas` (
 
     INDEX `id_administrador`(`id_administrador`),
     INDEX `id_evento`(`id_evento`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Rifa` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_sorteio` INTEGER NOT NULL,
+    `id_utilizador` INTEGER NULL,
+    `estado` ENUM('Vencedor', 'SegundoLugar', 'TerceiroLugar', 'Perdedor', 'PorComprar', 'Comprada') NOT NULL DEFAULT 'PorComprar',
+
+    INDEX `id_utilizador`(`id_utilizador`),
+    INDEX `id_sorteio`(`id_sorteio`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Pagamento` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_utilizador` INTEGER NOT NULL,
+    `id_sorteioRifas` INTEGER NOT NULL,
+    `quantidadeCompra` INTEGER NOT NULL,
+    `valor_total` DECIMAL(10, 2) NOT NULL,
+    `metodo_pagamento` ENUM('MBWAY', 'Entidade_Referencia') NOT NULL,
+    `estado` ENUM('Pendente', 'Pago', 'Falhado') NOT NULL DEFAULT 'Pendente',
+    `data_pagamento` TIMESTAMP(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    INDEX `id_utilizador`(`id_utilizador`),
+    INDEX `id_sorteioRifas`(`id_sorteioRifas`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -195,12 +195,6 @@ ALTER TABLE `CandidaturaVoluntariado` ADD CONSTRAINT `candidaturavoluntariado_ib
 ALTER TABLE `CandidaturaVoluntariado` ADD CONSTRAINT `candidaturavoluntariado_ibfk_2` FOREIGN KEY (`id_anuncio`) REFERENCES `Anuncio`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `RifaComprada` ADD CONSTRAINT `rifacomprada_ibfk_1` FOREIGN KEY (`id_utilizador`) REFERENCES `Utilizador`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
--- AddForeignKey
-ALTER TABLE `RifaComprada` ADD CONSTRAINT `rifacomprada_ibfk_2` FOREIGN KEY (`id_pagamento`) REFERENCES `Pagamento`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
--- AddForeignKey
 ALTER TABLE `Evento` ADD CONSTRAINT `evento_ibfk_1` FOREIGN KEY (`id_administrador`) REFERENCES `Administrador`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
@@ -219,13 +213,19 @@ ALTER TABLE `Notificacao` ADD CONSTRAINT `notificacao_ibfk_1` FOREIGN KEY (`id_u
 ALTER TABLE `Notificacao` ADD CONSTRAINT `notificacao_ibfk_2` FOREIGN KEY (`id_administrador`) REFERENCES `Administrador`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `Pagamento` ADD CONSTRAINT `pagamento_ibfk_1` FOREIGN KEY (`id_utilizador`) REFERENCES `Utilizador`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
--- AddForeignKey
-ALTER TABLE `Pagamento` ADD CONSTRAINT `pagamento_ibfk_2` FOREIGN KEY (`id_sorteioRifas`) REFERENCES `SorteioRifas`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
--- AddForeignKey
 ALTER TABLE `SorteioRifas` ADD CONSTRAINT `sorteio_rifas_ibfk_1` FOREIGN KEY (`id_administrador`) REFERENCES `Administrador`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE `SorteioRifas` ADD CONSTRAINT `sorteio_rifas_ibfk_2` FOREIGN KEY (`id_evento`) REFERENCES `Evento`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `Rifa` ADD CONSTRAINT `rifa_ibfk_2` FOREIGN KEY (`id_sorteio`) REFERENCES `SorteioRifas`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `Rifa` ADD CONSTRAINT `rifa_ibfk_1` FOREIGN KEY (`id_utilizador`) REFERENCES `Utilizador`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `Pagamento` ADD CONSTRAINT `pagamento_ibfk_1` FOREIGN KEY (`id_utilizador`) REFERENCES `Utilizador`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `Pagamento` ADD CONSTRAINT `pagamento_ibfk_2` FOREIGN KEY (`id_sorteioRifas`) REFERENCES `SorteioRifas`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
