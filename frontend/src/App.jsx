7c9d1f0278fcf293d64d311998360edeada45770
D1ogoAntunes <diogo.p.antunes04@gmail.com>
2025-05-07 15:21:38 +0100

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Registo from "./pages/registo"
 import Eventos from "./pages/eventos";
 import AnunciosAdmin from "./pages/voluntariadoAdmin";
 import "./styles/App.css"
+import MenuAdministrador from "./pages/menuAdministradores";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/menu" element={<Menu />} />
+          <Route path="/menuAdministrador" element={<MenuAdministrador />} />
           <Route path="/registo" element={<Registo />} />
           <Route path="/eventos" element={<Eventos />} />
           <Route path="/voluntariado" element={<AnunciosAdmin />} />
