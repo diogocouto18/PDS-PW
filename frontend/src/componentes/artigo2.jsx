c30030eeff4f827f8d9ab31b4e6fc404746c6830
r1cardoFerreira <ricardoferreira1510@outlook.pt>
2025-05-11 20:02:56 +0100

    Artigos

--- frontend/src/componentes/artigo2.jsx
+++ frontend/src/componentes/artigo2.jsx
@@ -7,6 +7,23 @@ const Artigo2 = ({ setShowArtigo2 }) => {
             <div className="modal">
                 <button className="close-btn" onClick={() => setShowArtigo2(false)}>×</button>
                 <h1>Artigo nº2</h1>
+                <p>
+                    Acesse a aplicação e vá até "Eventos":
+                    <br/><br/>
+                    Explore os eventos disponíveis na sua vizinhança.
+                    <br/><br/>  
+                    <b>Escolha o evento de interesse:</b> Clique no evento que deseja participar.
+                    <br/><br/>
+                    Clique em “Candidatar-se” ou “Participar”:
+                    <br/><br/>
+                    Se o evento exigir aprovação, sua candidatura será enviada ao organizador.
+                    <br/><br/>
+                    Caso não exija, você será confirmado automaticamente.
+                    <br/><br/> 
+                    Acompanhe o status: Você pode ver o status da sua candidatura no menu "Minhas Candidaturas".
+                    <br/><br/>
+                    <b>Nota:</b> Alguns eventos têm vagas limitadas, então candidate-se o quanto antes!
+                </p>
             </div>
         </div>
         )
--- frontend/src/componentes/artigo3.jsx
+++ frontend/src/componentes/artigo3.jsx
@@ -7,6 +7,23 @@ const Artigo3= ({ setShowArtigo3 }) => {
             <div className="modal">
                 <button className="close-btn" onClick={() => setShowArtigo3(false)}>×</button>
                 <h1>Artigo nº3</h1>
+                <p>
+                    Acesse sua conta e vá em “Meus Eventos”.
+                    <br/><br/>
+                    Clique no evento desejado.
+                    <br/><br/>
+                    Vá até a aba “Candidaturas”: Aqui você verá a lista de vizinhos que se candidataram.
+                    <br/><br/>
+                    Aprovar ou Rejeitar:
+                    <br/><br/>
+                    Clique em “Aprovar” para confirmar a presença do vizinho.
+                    <br/><br/>
+                    Clique em “Rejeitar” caso não possa aceitá-lo no evento.
+                    <br/><br/>
+                    <b>Importante:</b> A decisão será notificada ao vizinho automaticamente.
+                    <br/><br/>
+                    <b>Dica extra:</b> Seja gentil nas mensagens se quiser enviar um recado ao candidato!
+                </p>
             </div>
         </div>
         )
